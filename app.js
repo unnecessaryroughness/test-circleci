e61b3e8802b1638923df1a361cbdcb53317e706a
Mark Tonks <marktonks75@gmail.com>
2016-06-18 18:37:42 +0100

    added rudimentary mongodb connectivity

--- app.js
+++ app.js
@@ -3,7 +3,29 @@
 'use strict';
 
 var express = require('express'),
-    app = express();
+    app = express(),
+    mongoose = require('mongoose');
+
+    var self = this;
+
+  //set up connection to mongodb
+      self.mongoose = mongoose;
+
+      mongoose.connection.on('open', function() {
+          console.log('Connected to Mongo server');
+      });
+
+      mongoose.connection.on('error', function(err) {
+          console.log('Could not connect to Mongo server');
+          console.log(err);
+      });
+
+      self.mongourl = (process.env.DKRMONGO_PORT_27017_TCP_ADDR)
+                        ? process.env.DKRMONGO_PORT_27017_TCP_ADDR+':'+process.env.DKRMONGO_PORT_27017_TCP_PORT
+                        : 'mongodb://localhost/';
+
+      self.mongoose.connect(self.mongourl);
+      console.log('connecting to database: ' + self.mongourl);
 
 app.set('views', 'views');
 app.set('view engine', 'jade');
